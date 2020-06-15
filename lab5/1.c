68b31cdb69e9379031b3c90ebdf6cb4e54a70e6b
Nishat Tasnim Ali <65561961+1834902591@users.noreply.github.com>
2020-06-15 15:22:38 +0600

    Update 1.c

--- lab5/1.c
+++ lab5/1.c
@@ -3,7 +3,10 @@
 
 #include<stdio.h>
 int main()
+    
 {
+    
+    
     int *p;
     int i, N;
     int array[100];
@@ -12,13 +15,13 @@ int main()
 
     scanf("%d",&N);
 
-    for(i=0; i<N; i++)
-        scanf("%d",(p+i));
+    for(i=0; i<N; i++){
+        scanf("%d",(p+i));}
 
     printf("\n");
 
-    for(i=0; i<N; i++)
-        printf("%d ",*(p+i));
+    for(i=0; i<N; i++){
+        printf("%d ",*(p+i));}
 
     return 0;
 }
